cc960b8be239737a71f1640019189c54ba1ce2a8
Ifeoluwa <iamsamnice@gmail.com>
2025-02-05 09:11:58 -0800

    AddWithdraw UI

--- finance/src/components/AddWithdraw.jsx
+++ finance/src/components/AddWithdraw.jsx
@@ -1,7 +1,12 @@
+import PropTypes from "prop-types";
 import IconCancel from "./icon/IconCancel";
 
-// eslint-disable-next-line react/prop-types
-function AddWithdraw({ addWithdrawModal, setAddWithdrawModal, closeModal }) {
+function AddWithdraw({
+  addWithdrawModal,
+  setAddWithdrawModal,
+  closeModal,
+  buttonFor,
+}) {
   return (
     <div
       className={`fixed top-0 z-20 bg-black/70 w-full h-screen justify-center items-center ${
@@ -11,17 +16,47 @@ function AddWithdraw({ addWithdrawModal, setAddWithdrawModal, closeModal }) {
       <div className="bg-white w-[560px] rounded-md py-5 px-8 flex flex-col gap-4">
         <div className="flex justify-between items-center">
           <p className="text-3xl font-bold">
-            {addWithdrawModal === "add"
-              ? "Add to 'Savings"
-              : "Withdraw from 'Savings"}
+            {buttonFor === "add"
+              ? "Add to 'Savings'"
+              : "Withdraw from 'Savings'"}
           </p>
           <IconCancel onClick={closeModal} />
         </div>
         <p className="text-zinc-600">
-          {addWithdrawModal === "add"
+          {buttonFor === "add"
             ? "As your budgets change, feel free to update your spending limits."
             : "If your saving targets change, feel free to update your pots"}
         </p>
+        <div className="flex flex-col gap-2">
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-zinc-500 font-semibold">New Amount</p>
+            <p className="text-3xl text-black font-bold">$559.00</p>
+          </div>
+          <div
+            className="w-full h-2 rounded-full overflow-hidden
+           bg-zinc-100"
+          >
+            <div className="w-1/2 h-full bg-black"></div>
+          </div>
+          <div className="flex justify-between items-center">
+            <p className="text-sm text-green-600 font-semibold">27.95%</p>
+            <p className="font-semibold text-zinc-500 text-sm">
+              Target of $2000
+            </p>
+          </div>
+        </div>
+        <div>
+          <p className="font-semibold text-zinc-500 text-sm mb-1">
+            {buttonFor === "add" ? "Amount to Add" : "Amount to Withdraw"}
+          </p>
+          <div className="w-full border border-black h-10 rounded-md py-1 px-5 flex">
+            <input type="text" disabled placeholder="$" className="w-3" />
+            <input
+              type="text"
+              className="w-full h-full outline-none px-2 font-semibold"
+            />
+          </div>
+        </div>
         <div>
           <button
             onClick={() => {
@@ -29,12 +64,19 @@ function AddWithdraw({ addWithdrawModal, setAddWithdrawModal, closeModal }) {
             }}
             className="bg-zinc-900 w-full p-3 rounded-md font-semibold text-zinc-200"
           >
-            Save Changes
+            {buttonFor === "add" ? "Confirm Addition" : "Confirm Withdrawal"}
           </button>
         </div>
       </div>
     </div>
   );
 }
+
+AddWithdraw.propTypes = {
+  addWithdrawModal: PropTypes.bool.isRequired,
+  setAddWithdrawModal: PropTypes.func.isRequired,
+  closeModal: PropTypes.bool.isRequired,
+  buttonFor: PropTypes.oneOf(["add", "withdraw"]),
+};
 
 export default AddWithdraw;
